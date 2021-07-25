c0737357bfe104cebb1ebc1e60016d9d5c24175d
Nathalia <nathy.felix.31@gmail.com>
2021-07-24 21:01:46 -0300

    first commit

---
+++ src/main/java/com/sistemaifnmg/sistemaifnmg/SistemaifnmgApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaifnmg.sistemaifnmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaifnmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaifnmgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaifnmg/sistemaifnmg/SistemaifnmgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaifnmg.sistemaifnmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaifnmgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
