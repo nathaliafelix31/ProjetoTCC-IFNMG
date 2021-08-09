a76d9e258178c448dbdf981c559cd81b92153ded
Nathalia <nathy.felix.31@gmail.com>
2021-08-09 10:13:09 -0300

    update

--- src/main/java/com/sistemaifnmg/sistemaifnmg/Models/CCCC.java
+++ src/main/java/com/sistemaifnmg/sistemaifnmg/Models/CCCC.java
@@ -1,48 +1,8 @@
 package com.sistemaifnmg.sistemaifnmg.Models;
 
-import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Version;
 
-@Entity
-@Table(name = "CCCC")
-@DiscriminatorValue(value = "1")
-public class CCCC extends Pessoa implements Serializable {
+public class CCCC {
 
-    private static final long serialVersionUID = 1L;
-
-    @Column(nullable = false, length = 250, unique = true)
-    private String email;
-
-
-    @Version
-    private int version;
-
-    public CCCC(){
-        super();
-        //this.setCargo(Cargo.CCCC);
-        this.email = "";
-        this.version = 1;
-    }
-
-    public CCCC(String nome, String email /*Cargo cargo*/){
-        super();
-        /*this.setCargo(Cargo.CCCC);
-        this.setNome(nome);*/
-        this.email = "";
-        this.version = 1;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
 
 }
