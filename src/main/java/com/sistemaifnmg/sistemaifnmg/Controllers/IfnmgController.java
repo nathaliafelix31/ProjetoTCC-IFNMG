495b8ca492b36416dfa9d5dac54115fd6517660a
Nathalia <nathy.felix.31@gmail.com>
2021-08-10 16:27:06 -0300

    update

--- src/main/java/com/sistemaifnmg/sistemaifnmg/Controllers/IfnmgController.java
+++ src/main/java/com/sistemaifnmg/sistemaifnmg/Controllers/IfnmgController.java
@@ -1,4 +1,14 @@
 package com.sistemaifnmg.sistemaifnmg.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class IfnmgController {
+    @RequestMapping(value="/telaInicial", method=RequestMethod.POST)
+    public String form(){
+
+        return "telaInicial";
+    }
 }
