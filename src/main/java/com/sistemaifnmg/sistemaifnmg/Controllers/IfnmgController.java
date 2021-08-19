7969002858842fc442f95dd5413d75ed0924076a
Nathalia <nathy.felix.31@gmail.com>
2021-08-19 10:08:20 -0300

    update

--- src/main/java/com/sistemaifnmg/sistemaifnmg/Controllers/IfnmgController.java
+++ src/main/java/com/sistemaifnmg/sistemaifnmg/Controllers/IfnmgController.java
@@ -11,4 +11,24 @@ public class IfnmgController {
 
         return "telaInicial";
     }
+    @RequestMapping(value="/novoContrato", method=RequestMethod.GET)
+    public String novoContrato(){
+
+        return "novoContrato";
+    }
+    @RequestMapping(value="/buscarContrato", method=RequestMethod.GET)
+    public String buscarContrato(){
+
+        return "buscarContrato";
+    }
+    @RequestMapping(value="/aVencer", method=RequestMethod.GET)
+    public String aVencer(){
+
+        return "aVencer";
+    }
+    @RequestMapping(value="/vencidos", method=RequestMethod.GET)
+    public String vencidos(){
+
+        return "vencidos";
+    }
 }
